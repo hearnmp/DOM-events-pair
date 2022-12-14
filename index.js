af10c45102ffb1a25caff01cb147eee8adb49e3c
aaron-su98 <mr.aaron.su@gmail.com>
2022-12-14 16:32:58 -0500

    Added magnifying glass section

--- index.js
+++ index.js
@@ -6,26 +6,29 @@ cookieButton.addEventListener('click', function () {
     // console.log("i work!");
     cookieCounter ++;
     const cookieCountTotal =document.querySelector("#cookieClick-count");
-    cookieCountTotal.textContent = cookieCounter
+    cookieCountTotal.textContent = cookieCounter;
 });
 
 // magnifying glass constants
 const biggerButton = document.querySelector('#bigger');
 const smallerButton = document.querySelector('#smaller');
 let paragraphText = document.querySelector('#text');
-paragraphText.style.fontSize = '20px'
+let paragraphSize = paragraphText.style.fontSize;
+paragraphText.style.fontSize = "20px";
 
 biggerButton.addEventListener('click', function () {
-    console.log("this works!");
-    if(paragraphText.style.fontSize = '20px'){
-        return paragraphText.style.fontSize = '50px'
+    // console.log("this works!");
+    paragraphSize++;
+    if(paragraphSize < 100){
+        return document.body.querySelector('#text').style.fontSize = paragraphSize + 'px';
     }
 
 })
 
 smallerButton.addEventListener('click', function () {
-    console.log("this also works!");
-    if(paragraphText.style.fontSize = '20px'){
-        return paragraphText.style.fontSize = '5px'
+    // console.log("this also works!");
+    paragraphSize--;
+    if(paragraphSize > 1){
+        return document.body.querySelector('#text').style.fontSize = paragraphSize + 'px';
     }
 })
