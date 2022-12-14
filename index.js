d6d8058186267f880d26b3201200286f53a071ec
aaron-su98 <mr.aaron.su@gmail.com>
2022-12-14 15:39:43 -0500

    Edited .js file and .html

--- index.js
+++ index.js
@@ -8,3 +8,24 @@ cookieButton.addEventListener('click', function () {
     const cookieCountTotal =document.querySelector("#cookieClick-count");
     cookieCountTotal.textContent = cookieCounter
 });
+
+// magnifying glass constants
+const biggerButton = document.querySelector('#bigger');
+const smallerButton = document.querySelector('#smaller');
+let paragraphText = document.querySelector('#text');
+paragraphText.style.fontSize = '20px'
+
+biggerButton.addEventListener('click', function () {
+    console.log("this works!");
+    if(paragraphText.style.fontSize = '20px'){
+        return paragraphText.style.fontSize = '50px'
+    }
+
+})
+
+smallerButton.addEventListener('click', function () {
+    console.log("this also works!");
+    if(paragraphText.style.fontSize = '20px'){
+        return paragraphText.style.fontSize = '5px'
+    }
+})
